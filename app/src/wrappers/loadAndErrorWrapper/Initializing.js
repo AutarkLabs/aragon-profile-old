bfb429a142cf25e32817752f3d04a84ba04e6ca2
rkzel <rkz@hyperlogos.net>
2019-05-15 01:38:39 +0200

    added loading eth animation for unlocking box

--- app/src/wrappers/loadAndErrorWrapper/Initializing.js
+++ app/src/wrappers/loadAndErrorWrapper/Initializing.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { AnimationLoading } from '../../assets/'
 
-const Initializing = props => {
-  return <div>Initializing</div>
-}
+const Initializing = () => (
+  <div
+    style={{
+      marginTop: '3rem',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+    }}
+  >
+    <div style={{ textAlign: 'center' }}>
+      <AnimationLoading />
+    </div>
+
+    <div style={{ marginTop: '1rem' }}>Initializing</div>
+  </div>
+)
 
 export default Initializing
