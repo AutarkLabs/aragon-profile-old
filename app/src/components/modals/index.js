2004c9a8b1d594dd3e4f581d4ae5862b9d81a6a2
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2019-05-17 08:48:30 -0400

    clean removeItem propTypes

--- app/src/components/modals/index.js
+++ app/src/components/modals/index.js
@@ -108,12 +108,7 @@ const UserInfoModal = ({ ethereumAddress }) => {
         <WorkHistoryModal workHistoryId={modal.id || key} {...props} />
       )}
       {modal.type === 'removeItem' && (
-        <RemoveItem
-          item={getFormValue(modal.itemType, modal.id)}
-          itemType={modal.itemType}
-          ethereumAddress={ethereumAddress}
-          onRemove={removeItem}
-        />
+        <RemoveItem itemType={modal.itemType} onRemove={removeItem} />
       )}
     </Modal>
   )
